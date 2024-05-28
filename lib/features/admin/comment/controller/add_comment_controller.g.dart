3c08e6db107bdb3650c9b95af31f5c005adbfcc4
user <m.hany@ps-tt.com>
2024-05-28 12:52:24 +0300

    manager add comment

---
+++ lib/features/admin/comment/controller/add_comment_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'add_comment_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$addCommentControllerHash() =>
+    r'f0e70b2cc084bcdf31e8c3843384bf4b7ad2b3a0';
+
+/// See also [AddCommentController].
+@ProviderFor(AddCommentController)
+final addCommentControllerProvider =
+    AutoDisposeNotifierProvider<AddCommentController, AddCommentState>.internal(
+  AddCommentController.new,
+  name: r'addCommentControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$addCommentControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AddCommentController = AutoDisposeNotifier<AddCommentState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
---
+++ lib/features/manager/home/presentation/controller/comments/controller/manager_comments_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'manager_comments_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$managerCommentsControllerHash() =>
+    r'a5fa41ba557ceda4ed4a0a48c33716779e7b0705';
+
+/// See also [ManagerCommentsController].
+@ProviderFor(ManagerCommentsController)
+final managerCommentsControllerProvider = AutoDisposeNotifierProvider<
+    ManagerCommentsController, ManagerCommentsState>.internal(
+  ManagerCommentsController.new,
+  name: r'managerCommentsControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$managerCommentsControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ManagerCommentsController = AutoDisposeNotifier<ManagerCommentsState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
